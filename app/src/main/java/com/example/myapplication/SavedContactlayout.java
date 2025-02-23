926b5e0668e4954838be25994c524e048f859fba
itsvishalugale <vishalugale1935@gmail.com>
2025-02-23 14:33:30 +0530

    energancy contact changes

--- app/src/main/java/com/example/myapplication/SavedContactlayout.java
+++ app/src/main/java/com/example/myapplication/SavedContactlayout.java
@@ -1,28 +1,28 @@
 package com.example.myapplication;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import com.google.android.material.navigation.NavigationView;
-
-public class SavedContactlayout extends Activity {
+public class SavedContactlayout extends AppCompatActivity {
 
     LinearLayout llSavedContacts;
 
-
+    Toolbar toolbar;
     TextView contactname,contactnumber;
     MediaPlayer mediaPlayer;
    @SuppressLint("MissingInflatedId")
@@ -30,22 +30,29 @@ public class SavedContactlayout extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.saved_contact_layout); // Use saved_contact_layout.xml
+       toolbar=findViewById(R.id.toolbar);
+       setSupportActionBar(toolbar);
+       getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        llSavedContacts = findViewById(R.id.ll_saved_contacts);
+       TextView toolbarTitle = findViewById(R.id.et_title);
+       toolbarTitle.setText("Welcome To HerSeafty");
+
+
+       llSavedContacts = findViewById(R.id.ll_saved_contacts);
 //        contactname=findViewById(R.id.tv_contact_name);
 //        contactnumber=findViewById(R.id.tv_contact_number);
-       Button sirenButton = findViewById(R.id.btn_siren);
+       //Button sirenButton = findViewById(R.id.btn_siren);
         mediaPlayer = MediaPlayer.create(this, R.raw.alert_sound);
-        sirenButton.setOnClickListener(v -> {
-            if (mediaPlayer != null) {
-                if (mediaPlayer.isPlaying()) {
-                    mediaPlayer.pause();
-                    mediaPlayer.seekTo(0);
-                } else {
-                    mediaPlayer.start();
-                }
-            }
-        });
+//        sirenButton.setOnClickListener(v -> {
+//            if (mediaPlayer != null) {
+//                if (mediaPlayer.isPlaying()) {
+//                    mediaPlayer.pause();
+//                    mediaPlayer.seekTo(0);
+//                } else {
+//                    mediaPlayer.start();
+//                }
+//            }
+//        });
     }
     public void openMedicalStock(View view) {
         Intent intent = new Intent(this, NearbyHospitalList.class);
@@ -73,5 +80,33 @@ public class SavedContactlayout extends Activity {
     public void openWhether(View view) {
         Intent intent = new Intent(getApplicationContext(), WeatherForecastActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=new MenuInflater(getApplicationContext());
+        menuInflater.inflate(R.menu.drawer_items,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+       int itemId=item.getItemId();
+       if(itemId==R.id.navLogout){
+           Toast.makeText(getApplicationContext(), "Logout successfully!", Toast.LENGTH_SHORT).show();
+           Intent i=new Intent(getApplicationContext(),LoginActivity.class);
+           startActivity(i);
+       }
+       if(itemId==R.id.navSieren){
+           if (mediaPlayer != null) {
+               if (mediaPlayer.isPlaying()) {
+                   mediaPlayer.pause();
+                   mediaPlayer.seekTo(0);
+               } else {
+                   mediaPlayer.start();
+               }
+           }
+       }
+        return super.onOptionsItemSelected(item);
     }
 }
